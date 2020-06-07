62a116af3f516948ca0e1dd1a5a22fa8720d0da6
mkirilly <miklos.kirilly@gmail.com>
2020-06-07 20:34:11 +0200

    Add empty "game activity" and button to start it
    * Add a "game" activity that does not do anything at the moment.
    * Replace the "Hello world!" text in the main activity with a button
    that starts this new activity.
    * Add in-app navigation that can take you back from the game to the
    main activity.

--- app/src/main/java/com/mkirilly/btmastermind/MainActivity.java
+++ app/src/main/java/com/mkirilly/btmastermind/MainActivity.java
@@ -2,7 +2,9 @@ package com.mkirilly.btmastermind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user touches the "start game" button, starts a game */
+    public void startGameClicked(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
 }
